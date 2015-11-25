b9478db2076ee50be7394dbd4d6e54831d80c8ce
linusdamyo <linusdamyo@gmail.com>
2015-11-25 11:10:13 +0900

    Previous versions did not work. Fixed.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var pageMod = require("sdk/page-mod").PageMod({
   include: "*",
-  contentStyle: "@import url('//fonts.googleapis.com/earlyaccess/nanumgothic.css');",
   contentStyleFile: require("sdk/self").data.url("korea_font_family.css")
 });
